99660bed052b50ebb8512e98a10522356b4c2a29
snake <kurano.shibainu@gmail.com>
2025-11-19 16:16:16 +0900

    camera comit

---
+++ src/Week06/camera.py
@@ -0,0 +1,18 @@
+from picamera2 import Picamera2
+from datetime import datetime
+
+def capture(height,width,cnt):
+    picam2=Picamera2()
+
+    config=picam2.create_still_configuration(main={"size":(width,height)})
+    picam2.configure(config)
+    
+    picam2.start()
+    
+    filename=""
+
+    picam2.capture_file(filename)
+
+    picam2.close()
+
+
